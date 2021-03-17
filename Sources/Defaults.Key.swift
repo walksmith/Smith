40545c8a41acbf75ee01fca29d27766f70f9086f
vaux <zero.griffin@gmail.com>
2021-03-17 15:47:55 +0100

    Add defaults

---
+++ Sources/Defaults.Key.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public extension Defaults {
+    enum Key: String {
+        case
+        rated,
+        created
+    }
+}
---
+++ Sources/Defaults.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+public final class Defaults: UserDefaults {
+    public class var rated: Bool {
+        get { self[.rated] as? Bool ?? false }
+        set { self[.rated] = newValue }
+    }
+    
+    public class var created: Date? {
+        get { self[.created] as? Date }
+        set { self[.created] = newValue }
+    }
+    
+    private class subscript(_ key: Key) -> Any? {
+        get { standard.object(forKey: key.rawValue) }
+        set { standard.setValue(newValue, forKey: key.rawValue) }
+    }
+}
